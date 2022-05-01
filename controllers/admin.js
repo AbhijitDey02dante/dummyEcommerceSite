99208e7eb15448649f1ca25f42527e857424f789
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-05-01 15:37:13 +0530

    Association

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,11 +16,11 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title:title,
     imageUrl:imageUrl,
     description:description,
-    price:price
+    price:price,
   })
   .then((result)=>{
     console.log("Added Product");
@@ -30,7 +30,8 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
   .then(products => {
     res.render('admin/products', {
       prods: products,
@@ -43,14 +44,16 @@ exports.getProducts = (req, res, next) => {
 
 
 exports.getEditProduct = (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
   const editMode = req.query.edit;
   if(!editMode){
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product =>{
+  req.user.getProducts({where:{id:prodId}})
+  // Product.findByPk(prodId)
+  .then(products =>{
+    const product = products[0];
     if(!product){
       return res.redirect('/');
     }
